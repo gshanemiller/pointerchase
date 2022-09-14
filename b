40f46e3736079506024a7b9ed2268e9c2bb27c18
Your Name <you@example.com>
2022-09-14 04:40:47 +0000

    add pmu

--- b
+++ b
@@ -1,1 +1,5 @@
-g++ -O3 -g -DNDEBUG t.cpp -o t
+set -x
+g++ -I. -O3 -g -c -DNDEBUG main.cpp -o main.o
+g++ -I. -O3 -g -c -DNDEBUG intel_pmu_stats.cpp -o intel_pmu_stats.o
+g++ -I. -O3 -g -c -DNDEBUG intel_skylake_pmu.cpp -o intel_skylake_pmu.o
+g++ *.o -o pc.tsk -lpthread
--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@
 #include <string>
 #include <vector>
 
+#include <intel_pmu_stats.h>
+#include <intel_skylake_pmu.h>
+
 const int NodeWidth = 122-65+1;
+Intel::SkyLake::PMU pmu(false, Intel::SkyLake::PMU::ProgCounterSetConfig::k_DEFAULT_SKYLAKE_CONFIG_0);
 
 template <int T>
 struct Node {
@@ -58,11 +62,12 @@ struct Node {
 };
 
 int main(int argc, char **argv) {
-  
   if (argc!=3) {
     printf("usage: t <count> <file>\n");
     exit(2);
   }
+
+  printf("sizeof(Node<%d>) == %lu\n", NodeWidth, sizeof(Node<NodeWidth>));
 
   std::vector<std::string*> str(atoi(argv[1]));
   printf("str capacity: %lu size %lu\n", str.capacity(), str.size());
@@ -88,24 +93,34 @@ int main(int argc, char **argv) {
   }
   fclose(fid);
 
+  timespec start, end;                                                                                                  
+  Intel::Stats findStats;
+  Intel::Stats insertStats;
   Node<NodeWidth> *root = new Node<NodeWidth>;
 
+  pmu.reset();
+  pmu.start();                                                                                                       
+  timespec_get(&start, TIME_UTC); 
   for (u_int32_t k=0; k<str.size(); ++k) {
     root->insert(str[k]->c_str(), str[k]->length());
-    if (k%100000==0) {
-      printf("inserted %u...\n", k);
-    }
   }
-  printf("%llu nodes created\n", root->nodeCount);
+  timespec_get(&end, TIME_UTC);                                                                                       
+  insertStats.record("Insert", str.size(), start, end, pmu);
+  insertStats.dumpScaled(pmu);
 
+  printf("%lu nodes created\n", root->nodeCount);
+
+  pmu.reset();
+  pmu.start();                                                                                                       
+  timespec_get(&start, TIME_UTC); 
   for (u_int32_t k=0; k<str.size(); ++k) {
     if (root->find(str[k]->c_str(), str[k]->length())!=0) {
       printf("error\n");
     }
-    if (k%100000==0) {
-      printf("searched %u...\n", k);
-    }
   }
+  timespec_get(&end, TIME_UTC);                                                                                       
+  findStats.record("Find", str.size(), start, end, pmu);
+  findStats.dumpScaled(pmu);
   
   return 0;
 }
